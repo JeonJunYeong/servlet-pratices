78bfea5804aa5d9d6031ee5b8c2d14aa8227bd23
bit <bit@DESKTOP-ULIDFS7>
2020-02-18 15:18:14 +0900

    tomcat maven plugin 추가

--- guestbook01/src/main/java/com/douzone/guestbook/dao/GuestbookDao.java
+++ guestbook01/src/main/java/com/douzone/guestbook/dao/GuestbookDao.java
@@ -150,18 +150,18 @@ public class GuestbookDao {
 	}
 
 	private Connection getConnection() throws SQLException {
+
 		Connection conn = null;
+		
 		try {
-			//1. 드라이버 로딩
-			Class.forName( "com.mysql.jdbc.Driver" );
+			Class.forName("org.mariadb.jdbc.Driver");
 			
-			//2. 연결하기
-			String url="jdbc:mysql://localhost/webdb";
+			String url = "jdbc:mysql://192.168.1.114:3307/webdb";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
-		} catch( ClassNotFoundException e ) {
-			System.out.println( "드러이버 로딩 실패:" + e );
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
 		} 
 		
 		return conn;
-	}	
+	}
 }
